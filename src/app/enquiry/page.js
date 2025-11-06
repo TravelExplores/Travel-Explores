49c613c86a25586d11269088aa43a34dd00d77ba
Kartikey <kartikeypathak08@gmail.com>
2025-11-06 20:10:42 +0530

    fixed some bugs

--- src/app/enquiry/page.js
+++ src/app/enquiry/page.js
@@ -1,14 +1,7 @@
-// /enquiry/page.jsx
+"use client"; // THIS MAKES THE ENTIRE PAGE CLIENT-SIDE
 
-"use client";
-
-import { Suspense } from "react";
-import EnquiryForm from "../../../components/Form"
+import EnquiryForm from "../../../components/Form";
 
 export default function EnquiryPage() {
-  return (
-    <Suspense fallback={<div className=" h-screen w-full max-w-screen flex items-center justify-center text-black">Loading...</div>}>
-      <EnquiryForm />
-    </Suspense>
-  );
+  return <EnquiryForm />;
 }
